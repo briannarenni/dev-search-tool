59711349237373fe5ccb7e0d831a3362e3cbadaf
Brianna Renni <briannaxrene@icloud.com>
2023-11-13 13:55:17 -0800

    Add Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<p class="footer">
+  Brianna Renni | <a
+    href="https://github.com/briannarenni/"
+    target="_blank">Github</a
+  >
+</p>
+
+<style>
+  .footer {
+    font-size: 12px;
+    text-align: center;
+  }
+
+  .footer a {
+    color: var(--action-text);
+  }
+
+  .footer a:hover {
+    color: var(--action-text);
+    font-weight: bold;
+  }
+</style>
