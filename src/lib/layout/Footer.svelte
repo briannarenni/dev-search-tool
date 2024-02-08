33a67c6e80993a5650607e3138d12787d52dcfd0
Brianna Renni <briannaxrene@icloud.com>
2024-02-08 14:48:01 -0800

    refactor: Add Github logo

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,7 +1,11 @@
+<script>
+  import { Github } from 'lucide-svelte';
+</script>
+
 <footer class="footer">
   <p>
     Made by Brianna Renni |
-    <a href="https://github.com/briannarenni/" target="_blank">Github ⭐️</a>
+    <a href="https://github.com/briannarenni/" target="_blank"><Github /></a>
   </p>
 </footer>
 
@@ -13,10 +17,13 @@
 
   .footer a {
     color: var(--action-text);
+    padding: 7px;
   }
 
   .footer a:hover {
     color: var(--action-text);
     font-weight: bold;
+    outline: 1px solid var(--action-text);
+    border-radius: 50%;
   }
 </style>
